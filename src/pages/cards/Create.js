5bdc295e65f39646f6b64a27c5bd62bb2b79da4d
ppcamp <p.augustocampos@gmail.com>
2021-06-15 16:44:30 -0300

    update: added buttons

--- src/pages/cards/Create.js
+++ src/pages/cards/Create.js
@@ -1,51 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
 import { CardEdit } from "components/cards/edit/index";
-import { Box, Button, Container, Grid, makeStyles } from "@material-ui/core";
-import { NavigateNext, NavigateBefore } from "@material-ui/icons";
+import { Box, Button, Container, makeStyles } from "@material-ui/core";
+import { NavigateNext, NavigateBefore, Save, Clear } from "@material-ui/icons";
+import green from "@material-ui/core/colors/green";
+import { red } from "@material-ui/core/colors";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   button: {
     margin: theme.spacing(1),
+  },
+  save: {
+    background: green[900],
+    color: "#fff",
+    "&:hover": {
+      backgroundColor: green["A700"],
+    },
+  },
+  cancel: {
+    background: red[900],
+    color: "#fff",
+    "&:hover": {
+      backgroundColor: red["A700"],
+    },
   },
 }));
 
 export const CardCreate = (props) => {
   const classes = useStyles();
 
-  const submitFront = () => {
-    console.log("Submit the front part");
+  const [name, setName] = useState("Frente");
+
+  const onClickNext = () => {
+    setName("Atrás");
   };
-  const submitBack = () => {
-    console.log("Submit the back part");
+  const onClickBack = () => {
+    setName("Frente");
   };
+
+  const submit = () => {};
+  const cancel = () => {};
 
   return (
     <Container>
-      <CardEdit name='Frente' />
+      <CardEdit name={name} />
       <Box p={2} textAlign='center'>
         <Button
-          aria-label='upload picture'
+          aria-label='cancel this action'
+          component={Link}
+          variant='contained'
+          onClick={cancel}
+          className={`${classes.button} ${classes.cancel}`}
+          startIcon={<Clear />}
+          color='primary'
+          to='/'
+        >
+          Cancelar
+        </Button>
+
+        <Button
+          aria-label='goes back'
           component='span'
           variant='contained'
-          color='secondary'
-          onClick={() => console.log("test")}
+          onClick={onClickBack}
           className={classes.button}
           startIcon={<NavigateBefore />}
-          disabled
+          color='primary'
+          disabled={name === "Frente"}
         >
           Voltar
         </Button>
 
         <Button
-          aria-label='upload picture'
+          aria-label='write the back part'
           component='span'
           variant='contained'
-          color='secondary'
-          onClick={() => console.log("test")}
+          onClick={onClickNext}
           className={classes.button}
+          color='primary'
           startIcon={<NavigateNext />}
+          disabled={name === "Atrás"}
         >
           Avançar
+        </Button>
+        <Button
+          aria-label='save in the database'
+          component='span'
+          variant='contained'
+          onClick={submit}
+          className={`${classes.button} ${classes.save}`}
+          startIcon={<Save />}
+          disabled={name === "Frente"}
+        >
+          Salvar
         </Button>
       </Box>
     </Container>
