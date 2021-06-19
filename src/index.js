200a91f9d384d384de664d88f14aafd8e849dd12
ppcamp <p.augustocampos@gmail.com>
2021-06-19 18:07:46 -0300

    fix: change main tag

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementsByTagName("main")[0]
 );
 
 // If you want to start measuring performance in your app, pass a function
