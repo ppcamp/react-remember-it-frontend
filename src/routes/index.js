dffba99da5df769ec8d400c60012ccbd4497dc29
ppcamp <p.augustocampos@gmail.com>
2021-06-19 18:34:24 -0300

    fix: route problem

--- src/routes/index.js
+++ src/routes/index.js
@@ -32,32 +32,27 @@ const protectedRoutes = [...cardRoutes];
  * @returns A router to every route element
  */
 export const Routing = () => {
-  const auth = useAuth();
+  const { isLogged } = useAuth();
 
   return (
     <Router>
       <Switch>
         {/* Protected routes will redirect when the user isn't logged */}
-        {protectedRoutes.map(({ path, component, ...props }, index) => (
-          <Route
-            exact
-            path={path}
-            key={index}
-            {...props}
-            render={({ location }) =>
-              auth.isLogged ? (
-                component
-              ) : (
-                <Redirect
-                  to={{
-                    pathname: "/",
-                    state: { from: location },
-                  }}
-                />
-              )
-            }
-          />
-        ))}
+        {protectedRoutes.map(({ path, component, ...props }, index) => {
+          if (isLogged) {
+            return (
+              <Route
+                exact
+                path={path}
+                key={index}
+                {...props}
+                component={component}
+              />
+            );
+          } else {
+            return <Redirect to={{ pathname: "/" }} />;
+          }
+        })}
 
         {/* Public routes are accessed by anyone */}
         {publicRoutes.map(({ path, component, ...props }, index) => (
