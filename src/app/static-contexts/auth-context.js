9859d466e17c3936e188a358d75f21fd58c52713
ppcamp <p.augustocampos@gmail.com>
2021-06-20 19:32:03 -0300

    update: added session/local token management

--- src/app/static-contexts/auth-context.js
+++ src/app/static-contexts/auth-context.js
@@ -1,16 +1,58 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useState } from "react";
 
-const initialValue = {
-  isLogged: true,
-  user: "",
+// Initial state is defined here just for IDE purposes
+const initialState = {
+  /**
+   * Aplication token (JWT)
+   */
+  token: "",
+
+  /**
+   * Refresh the token in the react app and in the browser storage
+   * By default, save the value in the session storage
+   * @param {string} token The value for the new token
+   * @param {boolean} [keepLogged] Store the data in the local section. DEFAULT IS FALSE
+   */
+  onLogin: (token, keepLogged) => {},
+
+  /**
+   * Clear the token from local (and context) storage
+   */
+  onLogout: () => {},
 };
 
-const AuthContext = createContext(initialValue);
+const AuthContext = createContext(initialState);
 
-export const AuthContextProvider = (props) => {
+export const AuthContextProvider = ({ children }) => {
+  // Reading local/session storage
+  const storageName = process.env.REACT_APP_TOKEN || "Token";
+  const local = localStorage.getItem(storageName);
+  const session = sessionStorage.getItem(storageName);
+
+  // State
+  const [authState, setAuthState] = useState({
+    token: local || session || "",
+    onLogin: () => {},
+    onLogout: () => {},
+  });
+
+  // Handlers
+  const onLogin = (token, keepLogged = false) => {
+    if (keepLogged) {
+      localStorage.setItem(storageName, token);
+    }
+    sessionStorage.setItem(storageName, token);
+    setAuthState((state) => ({ ...state, token }));
+  };
+  const onLogout = () => {
+    localStorage.removeItem(storageName);
+    sessionStorage.removeItem(storageName);
+    setAuthState((state) => ({ ...state, token: "" }));
+  };
+
   return (
-    <AuthContext.Provider value={initialValue}>
-      {props.children}
+    <AuthContext.Provider value={{ ...authState, onLogin, onLogout }}>
+      {children}
     </AuthContext.Provider>
   );
 };
