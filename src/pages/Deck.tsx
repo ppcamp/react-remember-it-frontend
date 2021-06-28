96933dbba4e297c506f4420c116f51e28e113f92
ppcamp <p.augustocampos@gmail.com>
2021-06-28 15:17:13 -0300

    update: added deck page

--- src/pages/Deck.tsx
+++ src/pages/Deck.tsx
@@ -1,8 +1,29 @@
 import React from "react";
+import { Box, Typography } from "@material-ui/core";
+import { MenuAppBar } from "components/topbar";
 import { useParams } from "react-router";
 import { RouteParams } from "scripts/shared-types";
 
 export const DeckPage = () => {
+  const title = "Deck title";
   const { id } = useParams<RouteParams>();
-  return <div>Deck #{id}</div>;
+  const description = `Deck #${id}`;
+
+  return (
+    <div>
+      <MenuAppBar />
+
+      <Box m={4} py={4}>
+        <Typography variant="h4" paragraph>
+          {title}
+        </Typography>
+        <Typography variant="body1" paragraph>
+          {description}
+        </Typography>
+      </Box>
+      <Box p={4}>
+        <Typography variant="h6">Cartões</Typography>
+      </Box>
+    </div>
+  );
 };
