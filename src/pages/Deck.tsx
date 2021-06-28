6fb1c0b1e32b30360ed4e86c681172b977dccfdc
ppcamp <p.augustocampos@gmail.com>
2021-06-28 15:46:18 -0300

    update: deck header

--- src/pages/Deck.tsx
+++ src/pages/Deck.tsx
@@ -1,26 +1,77 @@
 import React from "react";
-import { Box, Typography } from "@material-ui/core";
+import { Box, Grid, IconButton, Typography } from "@material-ui/core";
+import { DeleteForever, Settings } from "@material-ui/icons";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import { MenuAppBar } from "components/topbar";
-import { useParams } from "react-router";
 import { RouteParams } from "scripts/shared-types";
+import { useParams } from "react-router";
 import { useSelector } from "react-redux";
 import { RootState } from "store";
+import { useHistory } from "react-router-dom";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: "center",
+      color: theme.palette.text.secondary,
+    },
+  })
+);
 
 export const DeckPage = () => {
+  // History
+  const history = useHistory();
+
   const deck = useSelector((state: RootState) => state.deck);
+  const classes = useStyles();
   const { id } = useParams<RouteParams>();
+
+  const onClickSettings = () => {
+    // open settings modal
+  };
+  const onClickDelete = () => {
+    // deleted element
+
+    // open settings modal
+    history.push("/dashboard");
+  };
 
   return (
     <div>
       <MenuAppBar />
 
       <Box m={4} py={4}>
-        <Typography variant="h4" paragraph>
-          {deck.title}
-        </Typography>
-        <Typography variant="body1" paragraph>
-          {deck.description}
-        </Typography>
+        <div className={classes.root}>
+          <Grid
+            container
+            direction="row"
+            justify="space-between"
+            alignItems="flex-start"
+          >
+            <Grid item xs={8}>
+              <Typography variant="h4" paragraph>
+                {deck.title}
+              </Typography>
+              <Typography variant="body1" paragraph>
+                {deck.description}
+              </Typography>
+            </Grid>
+            <Grid item xs={2}>
+              <Box textAlign="right">
+                <IconButton onClick={onClickSettings}>
+                  <Settings />
+                </IconButton>
+                <IconButton onClick={onClickDelete}>
+                  <DeleteForever />
+                </IconButton>
+              </Box>
+            </Grid>
+          </Grid>
+        </div>
       </Box>
       <Box p={4}>
         <Typography variant="h6">Cartões</Typography>
